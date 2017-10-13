c7de4e0d5efc758c736e80359fa89c59c1517516
Chad Ostrowski <hi@chadoh.com>
2017-10-13 17:13:43 -0400

    Add html2pdf to save the page
    This uses the html2pdf library from
    https://github.com/eKoopmans/html2pdf, which is not published on npm.
    Hence the included script tags and the window global.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div ref={div => this.app = div} className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1>PDF Test</h1>
+          <button onClick={() => window.saveAsPdf(this.app, 'react-app')}>
+            Save this page as a PDF!
+          </button>
         </header>
-        <p className="App-intro">
+        <p>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
